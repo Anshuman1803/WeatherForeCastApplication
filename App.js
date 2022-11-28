e42fbd126f5470467e62bb68c09730fa4cc74a97
Anshuman Kumar <mahianshuman491@gmail.com>
2022-11-28 20:48:36 +0530

    Update App.js
    Fetching and show the icons of weather condition.

--- App.js
+++ App.js
@@ -16,48 +16,45 @@ let ErrorMsg = document.getElementById("ErrorMsg");
 
 
 let Day1= document.getElementById("Day1")
-let Day1Condition = document.getElementById("Day1Condition");
 let Day1Temperature = document.getElementById("Day1Temperature");
 let Day1Humidity = document.getElementById("Day1Humidity");
 let Day1Wind = document.getElementById("Day1Wind");
+let Day1ConditionBox = document.getElementById("Day1ConditionBox");
 
 let Day2= document.getElementById("Day2")
-let Day2Condition = document.getElementById("Day2Condition");
 let Day2Temperature = document.getElementById("Day2Temperature");
 let Day2Humidity = document.getElementById("Day2Humidity");
 let Day2Wind = document.getElementById("Day2Wind");
+let Day2ConditionBox = document.getElementById("Day2ConditionBox");
 
 let Day3= document.getElementById("Day3")
-let Day3Condition = document.getElementById("Day3Condition");
+let Day3ConditionBox = document.getElementById("Day3ConditionBox");
 let Day3Temperature = document.getElementById("Day3Temperature");
 let Day3Humidity = document.getElementById("Day3Humidity");
 let Day3Wind = document.getElementById("Day3Wind");
 
 
 let Day4= document.getElementById("Day4")
-let Day4Condition = document.getElementById("Day4Condition");
+let Day4ConditionBox = document.getElementById("Day4ConditionBox");
 let Day4Temperature = document.getElementById("Day4Temperature");
 let Day4Humidity = document.getElementById("Day4Humidity");
 let Day4Wind = document.getElementById("Day4Wind");
 
 
 let Day5= document.getElementById("Day5")
-let Day5Condition = document.getElementById("Day5Condition");
+let Day5ConditionBox = document.getElementById("Day5ConditionBox");
 let Day5Temperature = document.getElementById("Day5Temperature");
 let Day5Humidity = document.getElementById("Day5Humidity");
 let Day5Wind = document.getElementById("Day5Wind");
 
 
 let Day6= document.getElementById("Day6")
-let Day6Condition = document.getElementById("Day6Condition");
+let Day6ConditionBox = document.getElementById("Day6ConditionBox");
 let Day6Temperature = document.getElementById("Day6Temperature");
 let Day6Humidity = document.getElementById("Day6Humidity");
 let Day6Wind = document.getElementById("Day6Wind");
 
-
-
-
-// let WeatherConditionBox = document.getElementById("WeatherConditionBox");
+let ConditionIconBox = document.getElementById("ConditionIcon");
 
 
 SearchButton.addEventListener('click', (event)=>{
@@ -98,52 +95,64 @@ const GetWeather =  (city) =>{
     })
 
 }
+
 const ShowWeather = (WeatherData) =>{
-    // console.log(WeatherData);
+    console.log(WeatherData);
 
     City.innerHTML = `${WeatherData.location.name}`;
     Temperature.innerHTML =`${WeatherData.current.temp_c}`;
     Condition.innerHTML = `${WeatherData.current.condition.text}`;
     Humidity.innerHTML = `${WeatherData.current.humidity}`;
     WindSpeed.innerHTML = `${WeatherData.current.wind_kph}`;
+    ConditionIconBox.innerHTML = `<img src="${WeatherData.current.condition.icon}" alt="Condition-Icon">`
+    
 
     Day1.innerHTML = `${WeatherData.forecast.forecastday[1].date}`;
-    Day1Condition.innerHTML =  `${WeatherData.forecast.forecastday[1].day.condition.text}`;
+
+    Day1ConditionBox.innerHTML = `<p>Condition<span>${WeatherData.forecast.forecastday[1].day.condition.text}</span> </p>
+                                    <img src="${WeatherData.forecast.forecastday[1].day.condition.icon}" alt="Condition-Icon">`
+
     Day1Temperature.innerHTML =  `${WeatherData.forecast.forecastday[1].day.avgtemp_c}`+"&deg;C" ;
     Day1Humidity.innerHTML =  `${WeatherData.forecast.forecastday[1].day.avghumidity}` + "&percnt;";
     Day1Wind.innerHTML =  `${WeatherData.forecast.forecastday[1].day.maxwind_kph}` + "Km/h";
 
     Day2.innerHTML = `${WeatherData.forecast.forecastday[2].date}`;
-    Day2Condition.innerHTML =  `${WeatherData.forecast.forecastday[2].day.condition.text}`;
+    Day2ConditionBox.innerHTML = `<p>Condition<span>${WeatherData.forecast.forecastday[2].day.condition.text}</span> </p>
+                                    <img src="${WeatherData.forecast.forecastday[2].day.condition.icon}" alt="Condition-Icon">`
+
     Day2Temperature.innerHTML =  `${WeatherData.forecast.forecastday[2].day.avgtemp_c}`+"&deg;C";
     Day2Humidity.innerHTML =  `${WeatherData.forecast.forecastday[2].day.avghumidity}`+ "&percnt;";
     Day2Wind.innerHTML =  `${WeatherData.forecast.forecastday[2].day.maxwind_kph}`+ "Km/h";
 
     Day3.innerHTML = `${WeatherData.forecast.forecastday[3].date}`;
-    Day3Condition.innerHTML =  `${WeatherData.forecast.forecastday[3].day.condition.text}`;
+    Day3ConditionBox.innerHTML = `<p>Condition<span>${WeatherData.forecast.forecastday[3].day.condition.text}</span> </p>
+                                    <img src="${WeatherData.forecast.forecastday[3].day.condition.icon}" alt="Condition-Icon">`
     Day3Temperature.innerHTML =  `${WeatherData.forecast.forecastday[3].day.avgtemp_c}`+"&deg;C";
     Day3Humidity.innerHTML =  `${WeatherData.forecast.forecastday[3].day.avghumidity}`+ "&percnt;";
     Day3Wind.innerHTML =  `${WeatherData.forecast.forecastday[3].day.maxwind_kph}`+ "Km/h";
 
     Day4.innerHTML = `${WeatherData.forecast.forecastday[4].date}`;
-    Day4Condition.innerHTML =  `${WeatherData.forecast.forecastday[4].day.condition.text}`;
+    Day4ConditionBox.innerHTML = `<p>Condition<span>${WeatherData.forecast.forecastday[4].day.condition.text}</span> </p>
+    <img src="${WeatherData.forecast.forecastday[4].day.condition.icon}" alt="Condition-Icon">`
     Day4Temperature.innerHTML =  `${WeatherData.forecast.forecastday[4].day.avgtemp_c}`+"&deg;C";
     Day4Humidity.innerHTML =  `${WeatherData.forecast.forecastday[4].day.avghumidity}`+ "&percnt;";
     Day4Wind.innerHTML =  `${WeatherData.forecast.forecastday[4].day.maxwind_kph}`+ "Km/h";
 
     Day5.innerHTML = `${WeatherData.forecast.forecastday[5].date}`;
-    Day5Condition.innerHTML =  `${WeatherData.forecast.forecastday[5].day.condition.text}`;
+    Day5ConditionBox.innerHTML = `<p>Condition<span>${WeatherData.forecast.forecastday[5].day.condition.text}</span> </p>
+    <img src="${WeatherData.forecast.forecastday[5].day.condition.icon}" alt="Condition-Icon">`    
     Day5Temperature.innerHTML =  `${WeatherData.forecast.forecastday[5].day.avgtemp_c}`+"&deg;C";
     Day5Humidity.innerHTML =  `${WeatherData.forecast.forecastday[5].day.avghumidity}`+ "&percnt;";
     Day5Wind.innerHTML =  `${WeatherData.forecast.forecastday[5].day.maxwind_kph}`+ "Km/h";
 
     Day6.innerHTML = `${WeatherData.forecast.forecastday[6].date}`;
-    Day6Condition.innerHTML =  `${WeatherData.forecast.forecastday[6].day.condition.text}`;
+    Day6ConditionBox.innerHTML = `<p>Condition<span>${WeatherData.forecast.forecastday[6].day.condition.text}</span> </p>
+    <img src="${WeatherData.forecast.forecastday[6].day.condition.icon}" alt="Condition-Icon">`
     Day6Temperature.innerHTML =  `${WeatherData.forecast.forecastday[6].day.avgtemp_c}`+"&deg;C";
     Day6Humidity.innerHTML =  `${WeatherData.forecast.forecastday[6].day.avghumidity}`+ "&percnt;";
     Day6Wind.innerHTML =  `${WeatherData.forecast.forecastday[6].day.maxwind_kph}`+ "Km/h";
 
-    // WeatherConditionBox.append.innerHTML =`<img src="//cdn.weatherapi.com/weather/64x64/day/116.png" alt="Weather-Icon">`
+   
 };
 // .then(err => console.log(err));
 
